49b01d57f0daf5975120f2d6b0785af60dab3fa3
delk <delk>
2006-11-01 18:29:40 +0000

    Add comments - This file should be removed from CVS

--- vhd/addsub.vhd
+++ vhd/addsub.vhd
@@ -1,3 +1,24 @@
+-------------------------------------------------------------------------------
+-- Title      : Delay Register
+-- Project    : Public
+-------------------------------------------------------------------------------
+-- File       : addsub.vhd
+-- Author     : Kyriakos Deliparaschos (kdelip@mail.ntua.gr)
+-- Company    : NTUA/IRAL
+-- Created    : 01/11/06
+-- Last update: 01/11/06
+-- Platform   : Modelsim, Synplify, ISE
+-- Standard   : VHDL'93
+-------------------------------------------------------------------------------
+-- Description:
+-------------------------------------------------------------------------------
+-- Copyright (c) 2004 NTUA
+-------------------------------------------------------------------------------
+-- revisions  :
+-- date        version  author  description
+-- 01/11/06    1.1      kdelip  created
+-------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -16,19 +37,19 @@ entity addsub is
            );          
   
   port(
-	in_array	: in  array (natural range 0 to elite-1) of std_logic_vector( score_sz-1 downto 0 );
-	result		: out std_logic_vector( score_sz + log2(elite)-1 downto 0 )  
-	);
+  in_array  : in  array (natural range 0 to elite-1) of std_logic_vector( score_sz-1 downto 0 );
+  result    : out std_logic_vector( score_sz + log2(elite)-1 downto 0 )  
+  );
 end entity addsub;
 
 ARCHITECTURE rtl OF addsub IS
 
 
 BEGIN
-	PROCESS (in_array)
-	BEGIN
-		
-			
-		
-	END PROCESS;
+  PROCESS (in_array)
+  BEGIN
+    
+      
+    
+  END PROCESS;
 END rtl;
