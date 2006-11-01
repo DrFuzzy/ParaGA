79e5dd6ad250d5956b061fe06cca0ad92523a1e0
delk <delk>
2006-11-01 23:06:22 +0000

    - renamed signal 'done' to 'high' and changed type signal to constant and set to '1' - renamed signal 'done2' to 'done' - performed code beautification

--- vhd/obs.vhd
+++ vhd/obs.vhd
@@ -6,7 +6,7 @@
 -- Author     : George Doyamis & Kyriakos Deliparaschos 
 -- Company    : NTUA/IRAL
 -- Created    : 23/03/06
--- Last update: 23/03/06
+-- Last update: 02/11/06
 -- Platform   : Modelsim & Synplify & Xilinx ISE
 -- Standard   : VHDL'93
 -------------------------------------------------------------------------------
@@ -35,17 +35,16 @@ use work.arith_pkg.all;
 -- ENTITY
 -------------------------------------------------------------------------------
 entity obs is
+
   generic(
-           score_sz	: positive;
-           fit_limit	: positive 
-         );  
-           
-  port ( clk        : in  std_logic;  -- clock
-         rst_n      : in  std_logic;  -- reset (active low)
-         max_fit    : in  std_logic_vector(score_sz-1 downto 0); -- produced by fit_eval block
-	 fitlim_rd  : out std_logic;  -- fitness limit reached (done signal in state machine)
-	 rd 	    : out std_logic   -- obs block ready signal
-         );
+    score_sz  : positive;
+    fit_limit : positive); 
+  port(clk        : in  std_logic;  -- clock
+        rst_n     : in  std_logic;  -- reset (active low)
+        max_fit   : in  std_logic_vector(score_sz-1 downto 0);  -- produced by fit_eval block
+        fitlim_rd : out std_logic;  -- fitness limit reached (done signal in state machine)
+        rd        : out std_logic); -- obs block ready signal
+
 end entity obs;
 
 -------------------------------------------------------------------------------
@@ -53,34 +52,32 @@ end entity obs;
 -------------------------------------------------------------------------------
 architecture rtl of obs is
 
-  signal done  : std_logic:= '0';
-  signal done2 : std_logic:= '0';
-  signal temp  : std_logic_vector(score_sz-1 downto 0);
+  constant high : std_logic := '1';
+  signal   done : std_logic;
+
 begin
 
-process (clk, rst_n)
-begin
-	if (rst_n = '0') then
-		rd <= '0';        
-		fitlim_rd <= '0';
-	elsif clk = '1' and clk'event then
-		rd <= done;
-		fitlim_rd <= done2;	
-	end if;
-end process;
+  process (clk, rst_n)
+  begin
+    if (rst_n = '0') then
+      rd        <= '0';
+      fitlim_rd <= '0';
+    elsif clk = '1' and clk'event then
+      rd        <= high;
+      fitlim_rd <= done;
+    end if;
+  end process;
 
-
-process (max_fit)
-begin
-	
-	temp<= conv_std_logic_vector(fit_limit, score_sz);
-	if max_fit < conv_std_logic_vector(fit_limit, score_sz) then 
-		done2 <= '0';
-	elsif max_fit = conv_std_logic_vector(fit_limit, score_sz) or max_fit > conv_std_logic_vector(fit_limit, score_sz) then 
-		done2 <= '1';
-	end if ;
-	done <= '1';
-	
-end process;
+  process (max_fit)
+  begin
+    
+    if max_fit < conv_std_logic_vector(fit_limit, score_sz) then
+      done <= '0';
+    elsif max_fit = conv_std_logic_vector(fit_limit, score_sz) or 
+          max_fit > conv_std_logic_vector(fit_limit, score_sz) then
+      done <= '1';
+    end if;
+    
+  end process;
 
 end rtl;
